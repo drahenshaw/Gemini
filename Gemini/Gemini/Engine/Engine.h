fa987f3a26220bc5deeb1b94fb20083bb4dc7a7c
David <drahenshaw@gmail.com>
2019-01-23 11:11:14 -0800

    Added packages for GLFW

---
+++ Gemini/Gemini/Engine/Engine.h
@@ -0,0 +1,22 @@
+#ifndef GEMINI_ENGINE
+#define GEMINI_ENGINE
+
+#include "GLFW/glfw3.h"
+
+class Engine
+{
+public:
+	Engine();
+	~Engine();
+
+	void StartUp();
+
+private:
+
+
+};
+
+
+
+
+#endif 
